60a75ebd56d6b6f2030f372926f3ed6938914b32
Gavin Jones <gjones@powerfarming.co.nz>
2019-08-19 13:26:44 +1200

    Tweak MinIO client to mc.exe instead of minio.exe

--- minio-client/tools/chocolateyinstall.ps1
+++ minio-client/tools/chocolateyinstall.ps1
@@ -7,8 +7,8 @@ $packageArgs = @{
   unzipLocation   = $toolsDir
   fileType        = 'EXE'
   url64bit        = $url64
-  FileFullPath    = "$toolsDir\minio.exe"
-  softwareName    = 'minio-server*'
+  FileFullPath    = "$toolsDir\mc.exe"
+  softwareName    = 'minio-client*'
 
   checksum64    = 'bd8d4f31d2e8a3781cf4ef891511beba05547ff029ef486955840f2af60f8d57'
   checksumType64  = 'sha256'
